0b7b9b1546abda56a871577a32cb425bf8cb85ae
fedeGalvan <trosonpvp@gmail.com>
2020-12-08 12:34:57 -0300

    test

--- src/jueguito/Vidas.java
+++ src/jueguito/Vidas.java
@@ -38,37 +38,5 @@ public class Vidas {
 	public void setColor(Color colorRecibido) {
 		color = colorRecibido;
 	}
-	    private int posicionX;
-	    private int posicionY;
-	    private Font font;
-	    private Color color;
-	    private int vidas;
-
-	    public Vidas(int posicionX, int posicionY, Font font, Color color, int vidas) {
-	        this.posicionX = posicionX;
-	        this.posicionY = posicionY;
-	        this.font = font;
-	        this.color = color;
-	        this.vidas = vidas;
-	    }
-
-	    public void dibujar(Graphics v) {
-			v.fillRect(469, 5, 101, 25);
-			v.setColor(color);
-			v.setFont(font);
-	        v.drawString("Vidas: " + String.valueOf(vidas), posicionX, posicionY);
-	    }
-
-	    public void perderVida() {
-	        vidas--;
-	    }
-
-	    public int getVidas() {
-	        return vidas;
-	    }
-
-	    public void setColor(Color colorRecibido) {
-	    	color = colorRecibido;
-	    }
-
+	    
 }
